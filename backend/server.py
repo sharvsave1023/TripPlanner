105308271aefa7485e3f87122740b7785b4c75ee
sharvsavetheworld <sharv.save@gmail.com>
2023-11-08 03:51:58 -0600

    backend
    did the backend

--- backend/server.py
+++ backend/server.py
@@ -1,46 +1,48 @@
-from flask import Flask, render_template, request
+#from flask import Flask, render_template, request
 import pandas as pd
 import requests as rq
+
+from flask import Flask, abort, current_app, request, render_template
+import json
 
 app = Flask(__name__)
 
 api_key = 'fXEzJ1nKIxwyo9y2PBVAoQ'
 url = 'https://www.carboninterface.com/api/v1/estimates'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def calculate_emissions():
-    if request.method == 'POST':
-        departure_airport1 = request.form['departure_airport1']
-        destination_airport1 = request.form['destination_airport1']
-        departure_airport2 = request.form['departure_airport2']
-        destination_airport2 = request.form['destination_airport2']
+    data = request.get_json()
+    airport_1 = data['airport_1']
+    airport_2 = data['airport_2']
 
-        headers = {
-            'Authorization': f'Bearer {api_key}',
-            'Content-Type': 'application/json'
-        }
+    headers = {
+        'Authorization': f'Bearer {api_key}',
+        'Content-Type': 'application/json'
+    }
 
-        data = {
-            "type": "flight",
-            "passengers": 2,
-            "legs": [
-                {"departure_airport": departure_airport1, "destination_airport": destination_airport1},
-                {"departure_airport": departure_airport2, "destination_airport": destination_airport2}
-            ]
-        }
+    data = {"type": "flight","passengers": 2,"legs": [{"departure_airport": airport_1, "destination_airport": airport_2}]
+}
+    response = rq.post(url, headers=headers, json=data)
+    result = response.json()
 
-        response = rq.post(url, headers=headers, json=data)
-        result = response.json()
-        data = result['data']['attributes']
+    data = result['data']['attributes']
+    df = pd.DataFrame(data = data)
+    carbon_emission_flight = "Carbon Emissions (grams): " + str(df['carbon_g'][0])
 
-        df = pd.DataFrame(data=data)
+    # Convert the dictionary to a JSON-formatted string
+    json_data = json.dumps(carbon_emission_flight)
 
-        carbon_emission_flight = df['carbon_g'][0]
-        carbon_emission_return = df['carbon_g'][1]
+        # Set the appropriate response headers for JSON data
+    response = app.response_class(
+        response=json_data,
+        status=200,
+        mimetype='application/json'
+    )
 
-        return render_template('index.html', carbon_emission_flight=carbon_emission_flight, carbon_emission_return=carbon_emission_return)
+    return response
 
-    return render_template('index.html')
+    
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 5500, host = '127.0.0.1')
+    app.run(host = '127.0.0.1', port = 5500, debug = True)
--- backend/test files/v0.py
+++ backend/test files/v0.py
@@ -8,24 +8,23 @@ url = 'https://www.carboninterface.com/api/v1/estimates'
 departure_airport1 = input("Enter Departing Airport 1: ")
 destination_airport1 = input("Enter Destination Airport 1: ")
 
-departure_airport2 = input("Enter Departing Airport 2: ")
-destination_airport2 = input("Enter Destination Airport 2: ")
+#departure_airport2 = input("Enter Departing Airport 2: ")
+#destination_airport2 = input("Enter Destination Airport 2: ")
 
 headers = {
     'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'
 }
 
-data = {"type": "flight","passengers": 2,"legs": [{"departure_airport": departure_airport1, "destination_airport": destination_airport1},
-        {"departure_airport": departure_airport2, "destination_airport": destination_airport2}
-    ]
+data = {"type": "flight","passengers": 2,"legs": [{"departure_airport": departure_airport1, "destination_airport": destination_airport1}]
 }
 
 response = rq.post(url, headers=headers, json=data)
 result = response.json()
+print(result)
 
 data = result['data']['attributes']
 
 df = pd.DataFrame(data = data)
 
 print("Carbon Emissions To Flight (Grams): ", df['carbon_g'][0])
-print("Carbon Emissions Return Flight (Grams): ", df['carbon_g'][1])
+#print("Carbon Emissions Return Flight (Grams): ", df['carbon_g'][1])
