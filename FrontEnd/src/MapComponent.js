147d14f9a83a089a581423f985a065471b2258bc
nxp-22 <nandinipaidesetty@gmail.com>
2023-11-09 22:29:01 -0600

    fixed path
    changed path from local

--- FrontEnd/src/MapComponent.js
+++ FrontEnd/src/MapComponent.js
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-import customMarkerIcon from '/Users/nandinipaidesetty/Documents/GitHub/TripPlanner/FrontEnd/src/Dev marker.png'; // Import your custom marker image
+import customMarkerIcon from './Dev marker.png'; // Import your custom marker image
 
 
 const airportCoords = {
